5bf9b412eacb48271bbb8f750be6d5058e7ed1bd
emanuel dos santos kanetchny <emanuel.skanis@hotmail.com>
2024-11-04 17:13:46 -0300

    Prototipo 0.1 tela (#4)

---
+++ components/style/blockLogin.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const blockLogin = StyleSheet.create({
+  blockLogin1: {
+    //position: "absolute",
+    height: 650,
+    width: 412,
+    borderTopLeftRadius: 55,
+    top: 240,
+    padding: 18,
+    backgroundColor: "white",
+  },
+  blockLogin2: {
+    position: "absolute",
+    top: 80,
+    backgroundColor: "#ffffff",
+    paddingBottom: 58.4,
+  },
+
+  blockLogin3: {
+    padding: 32,
+  },
+});
+
+export default blockLogin;
---
+++ components/style/buttonLogin.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const buttonLogin = StyleSheet.create({
+  buttonLogin: {
+    padding: 14,
+    backgroundColor: "#000000",
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  textButton: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
+
+export default buttonLogin;
